8fc434244fc9fa72d2dbee7d90b02ac1b1ff0f9a
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-27 22:17:10 +0530

    Create api-trigger.ps1

---
+++ api-trigger.ps1
@@ -0,0 +1,24 @@
+# Define your API endpoint URL
+$apiUrl = "https://httpbin.org/get"
+
+# Define the request headers (if needed)
+$headers = @{
+    "Content-Type" = "application/json"
+    # Add any other headers here if required
+}
+
+# Send a POST request to the API endpoint
+$response = Invoke-RestMethod -Uri $apiUrl -Method:Get -Headers $headers
+
+# Print the response (optional)
+Write-Host "API Response:"
+Write-Host $response
+
+# Check if the API call was successful (you can customize this condition)
+if ($response.Status -eq "Success") {
+    Write-Host "API call was successful."
+    exit 0  # Mark the workflow as successful
+} else {
+    Write-Host "API call failed."
+    exit 1  # Mark the workflow as failed
+}
