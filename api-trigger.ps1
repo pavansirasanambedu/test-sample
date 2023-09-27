aa2426c20c1e49acb8db9c7198bc0727b0c8f640
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-27 22:36:38 +0530

    Update api-trigger.ps1

--- api-trigger.ps1
+++ api-trigger.ps1
@@ -12,5 +12,5 @@ $response = Invoke-RestMethod -Uri $apiUrl -Method:Get -ContentType "application
 
 # Print the response (optional)
 Write-Host "API Response:"
-Write-Host $response | ConvertTo-Json
+Write-Host $response | ConvertFrom-Json
 
