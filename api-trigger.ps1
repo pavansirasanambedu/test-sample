214901a8a951580888e8b4d0610f484d7e0f8905
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-27 22:20:27 +0530

    Update api-trigger.ps1

--- api-trigger.ps1
+++ api-trigger.ps1
@@ -8,7 +8,7 @@ $headers = @{
 }
 
 # Send a POST request to the API endpoint
-$response = Invoke-RestMethod -Uri $apiUrl -Method:Get
+$response = Invoke-RestMethod -Uri $apiUrl -Method GET
 
 # Print the response (optional)
 Write-Host "API Response:"
